54aecda759ab14f0a41533aeeb3a9ca5a07f789c
Mouhanad Ahmed <houdacris88@gmail.com>
2025-05-12 09:33:29 +0300

    Login modal

--- src/componenets/navbar/Navbar.tsx
+++ src/componenets/navbar/Navbar.tsx
@@ -3,9 +3,20 @@ import Image from "next/image";
 import phoneIcon from '../../../public/images/phoneIcon.png'
 import seachIcon from '../../../public/images/searchIcon.png'
 import styles from './styles.module.css'
+import Link from "next/link";
+import React  ,{ useRef } from "react";
+import { useForm } from 'react-hook-form';
 
 export default function Navbar () {
+  const dialogRef = useRef<HTMLDialogElement>(null);
 
+  
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data:any) => console.log(data);
+    console.log(errors);
+  const openDialog = () => {
+    dialogRef.current?.showModal();
+  }
 
 
     return (     <div className="flex justify-between px-12 items-center" >
@@ -19,14 +30,14 @@ export default function Navbar () {
 
 <div className="flex justify-between gap-8 items-center" >
      
-     <button className="font-montserrat text-2xl font-semibold cursor-pointer" onClick={()=> window.alert('search')}>   <Image
+     <button className="font-montserrat text-2xl font-semibold cursor-pointer" onClick={openDialog}>   <Image
               className="me-2"
               src={seachIcon}
               alt="CatCard logo"
               width={24}
               height={24}
             /></button>
-     <button className="font-montserrat text-2xl font-semibold cursor-pointer"  onClick={()=> window.alert('login')}>Login</button>
+     <button className="font-montserrat text-2xl font-semibold cursor-pointer"  onClick={openDialog}>Login</button>
      
       <p className={`font-anton ${styles.callButton}`}>
       <Image
@@ -40,8 +51,76 @@ export default function Navbar () {
       </p>
       
     </div>
+    <dialog ref={dialogRef} id="my_modal_2" className="modal border-none">
+  <div className="modal-box bg-white text-black rounded-none w-5/12 max-w-5xl pt-12 px-12">
+
+    <h3 className="text-5xl  mb-8 md:text-6xl lg:text-[80px] text-center font-anton uppercase py-4">
+      LOGIN
+    </h3>
+    <p className="py-2 mb-6 text-center font-montserrat text-lg md:text-3xl lg:text-[32px] font-medium">
+    Please login to proceed
+    </p>
+
+    <div className="flex flex-col md:flex-row px-4 md:px-8 gap-6 md:gap-8 items-center justify-center pb-8" >
+
+
+  
+    <form onSubmit={handleSubmit(onSubmit)} style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '16px',
+        padding: '24px',
+        // margin: '0 auto',
+        // backgroundColor: '#fefefe',
+        // borderRadius: '12px',
+        // boxShadow: '0 10px 25px rgba(0, 0, 0, 0.1)',
+        // fontFamily: 'Segoe UI, sans-serif',
+        // maxWidth: '600px',
+        width:'90vw'
+      }}>
+      <input type="text" placeholder="Username" {...register("Username", {required: true})} className="w-full border" style={{
+            borderRadius: '2vw',
+            padding: '1vw',
+            border:"0.1vw solid #B6B6B6",
+      }}/>
+      <input type="password" placeholder="Password" {...register} className="w-full border " style={{
+            borderRadius: '2vw',
+            padding: '1vw',
+            border:"0.1vw solid #B6B6B6",
+            marginBottom :"0.5vw"
+      }}/>
+
+      <button type="submit" style={{
+          padding: '1vw',
+          // backgroundColor: defaultValues ? '#ffa500' : '#4CAF50',
+          backgroundColor: '#B10229' ,
+          color: 'white',
+          border: 'none',
+          borderRadius: '2vw',
+          cursor: 'pointer',
+          fontWeight: 'bold',
+          transition: 'background-color 0.3s ease',
+          textTransform:"uppercase"
+        }}
+        // onMouseOver={(e) => (e.currentTarget.style.backgroundColor = `${defaultValues ? '#ffa501' : '#4CAF49'}`)}
+        // onMouseOut={(e) => (e.currentTarget.style.backgroundColor = `${defaultValues ? '#ffa500' : '#4CAF50'}`)}
+      >
+        {/* {defaultValues ? 'Update' : 'Submit'} */} Login
+      </button>
+    </form>
+
+
+    </div>
+  </div>
+
+  <form method="dialog" className="modal-backdrop">
+    <button>close</button>
+  </form>
+</dialog>
     </div>
 
-    
+
     )
 }
+
+
