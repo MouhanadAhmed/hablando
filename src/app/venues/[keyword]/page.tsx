f34c2530bfbae82ba54e98821312edbdb6f69dea
Mouhanad Ahmed <houdacris88@gmail.com>
2025-05-16 17:08:14 +0300

    update venues page

---
+++ src/app/venues/[keyword]/page.tsx
@@ -0,0 +1,44 @@
+"use client"
+import CallBtn from "@/componenets/callBtn/CallBtn"
+import Footer from "@/componenets/footer/footer"
+import Navbar from "@/componenets/navbar/Navbar"
+import Image from "next/image"
+import arrowLeft from "../../../../public/images/arrow-left.png"
+import Card from "@/componenets/card/Card"
+import Link from "next/link"
+import CardsWrapper from "@/componenets/cardsWrapper/cardsWrapper"
+import { useParams } from "next/navigation"
+
+
+
+
+
+
+export default function Venues() {
+  const params = useParams();
+  const keyword = params?.keyword as string;
+
+    return(
+        <>
+        <Navbar/>
+        <div className="flex items-center py-8 ">
+        <Link href="/catcard">
+                    <Image
+                        src={arrowLeft}
+                        width={70}
+                        height={78}
+                        alt="call to action"
+                        className="max-md:size-[8vw] object-contain  md:size-[5vw]"
+                    ></Image>
+            
+          </Link>
+        <h1 className="text-5xl 3xl:text-6xl uppercase  font-bebas-neue">{keyword}</h1>
+        </div>
+            <div className="flex justify-center flex-col md:justify-start items-center lg:items-start 3xl:px-24 ">
+        <CardsWrapper keyword={keyword}/>
+            </div>
+        <CallBtn/>
+        <Footer/>
+        </>
+    )
+}
