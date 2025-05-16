a16a6387023dc878c2b75f1c736b4dffe6a54c34
Mouhanad Ahmed <houdacris88@gmail.com>
2025-05-16 17:08:27 +0300

    link benefits to venues

--- src/componenets/benefits/Benefits.tsx
+++ src/componenets/benefits/Benefits.tsx
@@ -37,7 +37,7 @@ export default function Benefits() {
       <div className={` mx-auto pt-[70vw] md:pt-0 relative z-[20]`}>
         <div className="flex flex-wrap justify-center">
           <div className="hidden max-md:block relative  mr-[20vw] mt-[9vw] cursor-pointer">
-          <Link href="/venues"> 
+          <Link href="/venues/restaurants"> 
             <Image
               src="/images/benefits-1.png"
               alt="restaurants Image"
@@ -45,7 +45,7 @@ export default function Benefits() {
               height={300}
               loading="lazy"
               className={`max-md:w-[70vw] object-contain w-full md:w-1/8 ${styles.restaurantsImg}`}
-              onClick={()=>redirect('/venues')}
+              onClick={()=>redirect('/venues/restaurants')}
             />
             <span className={`absolute uppercase text-nowrap -top-[8vw] -translate-x-[45%] left-[50%] max-md:!tracking-[0.5vw]  font-bebasNeue text-[11vw] font-bold ${styles.benefitsTextShadow}`}>
               RESTAURANTS
@@ -60,7 +60,7 @@ export default function Benefits() {
               height={300}
               loading="lazy"
               className={`max-md:w-[70vw] object-contain w-full md:w-1/8 ${styles.restaurantsImg}`}
-              onClick={()=>redirect('/venues')}
+              onClick={()=>redirect('/venues/bookshops')}
             />
             <span className={`absolute uppercase text-nowrap -top-[8vw] -translate-x-[55%] left-[50%] max-md:!tracking-[0.5vw]  font-bebasNeue text-[12vw] font-bold ${styles.benefitsTextShadow}`}>
               BOOK SHOPS
@@ -69,12 +69,12 @@ export default function Benefits() {
           <div className="hidden max-md:block relative mr-[20vw] mt-[34vw]">
             <Image
               src="/images/benefits-3.png"
-              alt="restaurants Image"
+              alt="languages Image"
               width={180}
               height={300}
               loading="lazy"
               className={`max-md:w-[70vw] object-contain w-full md:w-1/8 ${styles.restaurantsImg}`}
-              onClick={()=>redirect('/venues')}
+              onClick={()=>redirect('/venues/languages')}
             />
             <span className={`absolute uppercase text-nowrap -top-[8vw] -translate-x-[50%] left-[50%] max-md:!tracking-[0.5vw]  font-bebasNeue text-[12vw] font-bold ${styles.benefitsTextShadow}`}>
               Languages
@@ -83,12 +83,12 @@ export default function Benefits() {
           <div className="hidden max-md:block relative ml-[15vw] mt-[32vw]">
             <Image
               src="/images/benefits-4.png"
-              alt="restaurants Image"
+              alt="beauty Image"
               width={180}
               height={300}
               loading="lazy"
               className={`max-md:w-[70vw] object-contain w-full md:w-1/8 ${styles.restaurantsImg}`}
-              onClick={()=>redirect('/venues')}
+              onClick={()=>redirect('/venues/beauty')}
             />
             <span className={`absolute uppercase text-nowrap -top-[8vw] -translate-x-[50%] left-[50%] max-md:!tracking-[0.5vw]  font-bebasNeue text-[12vw] font-bold ${styles.benefitsTextShadow}`}>
               beauty
@@ -97,12 +97,12 @@ export default function Benefits() {
           <div className="hidden max-md:block relative  mr-[20vw] mt-[32vw]">
             <Image
               src="/images/benefits-5.png"
-              alt="restaurants Image"
+              alt="beachclubs Image"
               width={180}
               height={300}
               loading="lazy"
               className={`max-md:w-[70vw] object-contain w-full md:w-1/8 ${styles.restaurantsImg}`}
-              onClick={()=>redirect('/venues')}
+              onClick={()=>redirect('/venues/beachclubs')}
             />
             <span className={`absolute uppercase text-nowrap -top-[8vw] -translate-x-[45%] left-[50%] max-md:!tracking-[0.5vw]  font-bebasNeue text-[12vw] font-bold ${styles.benefitsTextShadow}`}>
               beach clubs
@@ -111,12 +111,12 @@ export default function Benefits() {
           <div className="hidden max-md:block relative ml-[15vw] mt-[30vw]">
             <Image
               src="/images/benefits-6.png"
-              alt="restaurants Image"
+              alt="fitness Image"
               width={180}
               height={300}
               loading="lazy"
               className={`max-md:w-[70vw] object-contain w-full md:w-1/8 ${styles.restaurantsImg}`}
-              onClick={()=>redirect('/venues')}
+              onClick={()=>redirect('/venues/fitness')}
             />
             <span className={`absolute uppercase text-nowrap -top-[8vw] -translate-x-[50%] left-[50%] max-md:!tracking-[0.5vw]  font-bebasNeue text-[12vw] font-bold ${styles.benefitsTextShadow}`}>
               fitness
@@ -125,12 +125,12 @@ export default function Benefits() {
           <div className="hidden max-md:block relative mr-[20vw] mt-[40vw]">
             <Image
               src="/images/benefits-7.png"
-              alt="restaurants Image"
+              alt="mealplan Image"
               width={180}
               height={300}
               loading="lazy"
               className={`max-md:w-[70vw] object-contain w-full md:w-1/8 ${styles.restaurantsImg}`}
-              onClick={()=>redirect('/venues')}
+              onClick={()=>redirect('/venues/mealplan')}
             />
             <span className={`absolute uppercase text-nowrap -top-[8vw] -translate-x-[50%] left-[50%] max-md:!tracking-[0.5vw]  font-bebasNeue text-[12vw] font-bold ${styles.benefitsTextShadow}`}>
               Meal plan
@@ -139,12 +139,12 @@ export default function Benefits() {
           <div className="hidden max-md:block relative ml-[15vw] mt-[50vw]">
             <Image
               src="/images/benefits-8.png"
-              alt="restaurants Image"
+              alt="wellness Image"
               width={180}
               height={300}
               loading="lazy"
               className={`max-md:w-[70vw] object-contain w-full md:w-1/8 ${styles.restaurantsImg}`}
-              onClick={()=>redirect('/venues')}
+              onClick={()=>redirect('/venues/wellness')}
             />
             <span className={`absolute uppercase text-nowrap -top-[8vw] -translate-x-[50%] left-[50%] max-md:!tracking-[0.5vw]  font-bebasNeue text-[12vw] font-bold ${styles.benefitsTextShadow}`}>
               wellness
@@ -153,12 +153,12 @@ export default function Benefits() {
           <div className="hidden max-md:block relative mr-[20vw] mt-[50vw]">
             <Image
               src="/images/benefits-9.png"
-              alt="restaurants Image"
+              alt="entertainment Image"
               width={180}
               height={300}
               loading="lazy"
               className={`max-md:w-[70vw] object-contain w-full md:w-1/8 ${styles.restaurantsImg}`}
-              onClick={()=>redirect('/venues')}
+              onClick={()=>redirect('/venues/entertainment')}
             />
             <span className={`absolute uppercase text-nowrap -top-[8vw] -translate-x-[45%] left-[50%] max-md:!tracking-[0.5vw]  font-bebasNeue text-[10vw] font-bold ${styles.benefitsTextShadow}`}>
               Entertainment
@@ -176,7 +176,7 @@ export default function Benefits() {
             className={`object-contain !w-[17vw] w-full ${styles.restaurantsImg} cursor-pointer`}
             data-aos="fade-up"
             data-aos-delay="700"
-            onClick={()=>redirect('/venues')}
+            onClick={()=>redirect('/venues/restaurants')}
           />
           {/* </Link> */}
           <Image
@@ -188,7 +188,7 @@ export default function Benefits() {
             className={`object-contain !w-[22vw] w-full pt-[25vw] md:w-1/6 ${styles.bookshopImg} cursor-pointer`}
             data-aos="fade-up"
           data-aos-delay="1000"
-          onClick={()=>redirect('/venues')}
+          onClick={()=>redirect('/venues/bookshop')}
           />
           <Image
             src="/images/languages.png"
@@ -199,7 +199,7 @@ export default function Benefits() {
             className={`w-full align-bottom !w-[17vw] object-contain pt-[45vw] md:w-1/8 ${styles.languagesImg} cursor-pointer`}
             data-aos="fade-up"
           data-aos-delay="1600"
-          onClick={()=>redirect('/venues')}
+          onClick={()=>redirect('/venues/languages')}
           />
         </div>
         <div className="hidden md:flex flex-wrap gap-16 md:gap-[4vw] justify-center items-center">
@@ -212,7 +212,7 @@ export default function Benefits() {
             className={`object-contain !w-[17vw] w-full ${styles.beautyImg} cursor-pointer`}
             data-aos="fade-up"
           data-aos-delay="1400"
-          onClick={()=>redirect('/venues')}
+          onClick={()=>redirect('/venues/beauty')}
           />
           <Image
             src="/images/beachClub.png"
@@ -223,7 +223,7 @@ export default function Benefits() {
             className={`object-contain !w-[17vw] pb-[25vw] w-full ${styles.beachImg} cursor-pointer`}
             data-aos="fade-up"
           data-aos-delay="1200"
-          onClick={()=>redirect('/venues')}
+          onClick={()=>redirect('/venues/beachclub')}
           />
           <Image
             src="/images/fitness.png"
@@ -234,7 +234,7 @@ export default function Benefits() {
             className={`object-contain !w-[22vw] w-full ${styles.fitnessImg} cursor-pointer`}
             data-aos="fade-up"
           data-aos-delay="1000"
-          onClick={()=>redirect('/venues')}
+          onClick={()=>redirect('/venues/fitness')}
           />
         </div>
         <div className={`hidden md:flex flex-wrap gap-16 md:gap-[4vw] justify-center ${styles.lastRow} cursor-pointer`}>
@@ -247,7 +247,7 @@ export default function Benefits() {
             className={`object-contain !w-[22vw] w-full ${styles.wellnessImg} cursor-pointer`}
             data-aos="fade-up"
           data-aos-delay="1000"
-          onClick={()=>redirect('/venues')}
+          onClick={()=>redirect('/venues/wellness')}
           />
           <Image
             src="/images/melPlan.png"
@@ -258,7 +258,7 @@ export default function Benefits() {
             className={`object-contain !w-[17vw] pb-[25vw] w-full ${styles.mealImg} cursor-pointer`}
             data-aos="fade-up"
           data-aos-delay="800"
-          onClick={()=>redirect('/venues')}
+          onClick={()=>redirect('/venues/mealplan')}
           />
           <Image
             src="/images/entertainment.png"
@@ -269,7 +269,7 @@ export default function Benefits() {
             className={`object-contain !w-[22vw] w-full ${styles.entertainmentImg} cursor-pointer`}
             data-aos="fade-up"
           data-aos-delay="1200"
-          onClick={()=>redirect('/venues')}
+          onClick={()=>redirect('/venues/entertainment')}
           />
         </div>
       </div>
