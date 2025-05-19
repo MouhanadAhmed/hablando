ba42241525a348579678debe360caa3ac597fdd5
omarzaeda <moraeslam1@gmail.com>
2025-05-20 01:26:49 +0300

    adding 2 headings, fix teacher issue

--- src/componenets/pushUp/PushUp.tsx
+++ src/componenets/pushUp/PushUp.tsx
@@ -7,6 +7,8 @@ const PushUp = () => {
   return (
     <div className=' max-md:w-[100vw] max-md:mt-[40vw] relative'>
       <div className="max-md:w-[100vw] max-md:max-w-[100vw] md:!h-[225vw] md:max-h-[225vw]">
+        <h1 data-aos="fade-left"
+          data-aos-duration="1500" className="max-md:hidden text-[5vw] pl-[40vw] font-montserrat font-bold pt-[25vw] top-0 absolute uppercase">AND <span className="text-[#B10229]">Much</span> MORE...</h1>
         <Image
           src="/images/pushUp.png"
           alt="Push up Image"
@@ -16,6 +18,8 @@ const PushUp = () => {
           className={`object-contain md:!pt-[65vw] relative z-50 max-md:!min-w-[120vw] max-md:!max-h-[190vw] md:w-[50vw]`}
         />
         <Image className="absolute max-md:hidden -right-[35%] top-[45%]" src="/images/main-bg.png" alt="Push up Image" width={5000} height={250} loading="lazy" />
+        <h1 data-aos="fade-left"
+          data-aos-duration="1500" className="max-md:hidden text-[5vw] pl-[35vw] font-montserrat font-bold pb-[35vw] bottom-0 absolute uppercase">To <span className="text-[#B10229]">start using</span> your CAT Card and its benefits!</h1>
       </div>
     </div>
   );
--- src/componenets/teacher/Teacher.tsx
+++ src/componenets/teacher/Teacher.tsx
@@ -1,112 +1,84 @@
-// import React, { useEffect } from "react";
-// import Image from "next/image";
-// import { gsap } from "gsap";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-// gsap.registerPlugin(ScrollTrigger);
-
-// const Teacher = () => {
-//   useEffect(() => {
-//     const teacherContainer = document.querySelector(".teacher-container");
-
-//     gsap.to(teacherContainer, {
-//       xPercent: -100, // Scroll horizontally from 0 to -100% (one full page)
-//       ease: "none",
-//       scrollTrigger: {
-//         trigger: teacherContainer,
-//         start: "bottom center", // Start scrolling from the middle of the image
-//         pin: true,
-//         scrub: 1,
-//         end: "+=100%", // End scroll after 200% of the container width
-//       },
-//     });
-//   }, []);
-
-//   return (
-//     <div className="teacher-container w-[100vw] pt-96 h-[100vh] mt-[80vh]">
-//       <div className="w-[100vw]">
-//         <Image
-//           src="/images/teacherImg.png"
-//           alt="Teacher Image"
-//           width={5000}
-//           height={250}
-//           loading="lazy"
-//           className="object-contain"
-//           style={{transform:"scale(0.9"}}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Teacher;
-
-
-
 "use client";
 import React, { useEffect } from "react";
 import Image from "next/image";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import Benefits from "../benefits/Benefits";
-import styles from './styles.module.css'
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Teacher = () => {
   useEffect(() => {
     if (window.innerWidth >= 768) {
-      const imagePanel = document.querySelector('.teacher-desktop-img-panel');
-      if (imagePanel) {
-        gsap.fromTo(
-          imagePanel,
-          { xPercent: 0 },
-          {
-            xPercent: -100,
-            ease: 'none',
-            scrollTrigger: {
-              trigger: imagePanel,
-              start: 'top 20%',
-              end: '+=200%',
-              scrub: true,
-              pin: true,
-              anticipatePin: 1,
-            },
+      const image = document.querySelector('.teacher-desktop-img-panel img');
+
+      if (image) {
+        image.addEventListener("load", () => {
+          const panel = document.querySelector(".teacher-desktop-img-panel");
+
+          if (panel) {
+            gsap.fromTo(
+              panel,
+              { xPercent: 0 },
+              {
+                xPercent: -100,
+                ease: "none",
+                scrollTrigger: {
+                  trigger: panel,
+                  start: "top 20%",
+                  end: "+=200%",
+                  scrub: true,
+                  pin: true,
+                  anticipatePin: 1,
+                },
+              }
+            );
           }
-        );
+        });
       }
     }
   }, []);
 
   return (
     <div className="outer-container md:h-[210vw] md:pt-[50vw] relative">
+      <Image
+        src="/images/teacherImg.png"
+        alt="Preload Teacher"
+        width={10}
+        height={10}
+        style={{ display: "none" }}
+        priority
+      />
+
       <div className="flex w-[200vw]">
-        {/* First Panel with Image */}
-        <div className="md:hidden teacher-container max-md:!-left-[20%] max-md:w-[130vw] z-[1000] max-md:absolute max-md:-left-[10vw] w-[100vw] flex items-center justify-center ">
+        <div className="md:hidden teacher-container max-md:!-left-[20%] max-md:w-[130vw] z-[1000] max-md:absolute max-md:-left-[10vw] w-[100vw] flex items-center justify-center">
           <Image
             src="/images/teacherImg.png"
             alt="Teacher Image"
             width={5000}
             height={250}
             loading="lazy"
-            className={`object-contain relative z-[1000] md:w-[100vw]`}
+            className="object-contain relative z-[1000] md:w-[100vw]"
             data-aos="fade-right"
             data-aos-duration="1000"
           />
           <div className="absolute w-[calc(100%-100px)] h-[250px] left-0 bottom-0 z-[1000] bg-gradient-to-t from-black to-transparent" />
         </div>
-        {/* Desktop Panel with horizontal scroll effect */}
-        <div className="hidden md:block teacher-desktop-img-panel" style={{ position: 'relative' }}>
+
+        <div
+          className="hidden md:block teacher-desktop-img-panel"
+          style={{ position: "relative", minHeight: "100vh" }}
+        >
           <Image
             src="/images/teacherImg.png"
             alt="Teacher Image"
             width={5000}
             height={250}
             loading="lazy"
-            className={`object-contain relative z-[1000] absolute bottom-[10vw] -z-[1] -left-[0] md:w-[100vw]`}
+            className="object-contain relative z-[1000] absolute bottom-[10vw] -z-[1] -left-[0] md:w-[100vw] will-change-transform opacity-100 transition-opacity duration-500"
           />
         </div>
-        {/* Second Panel - Placeholder Content */}
+
+        {/* Second Panel - Placeholder */}
         {/* <div className="w-[100vw]  flex items-center justify-center ">
           <p className="text-4xl font-bold text-black">Another Panel</p>
         <Benefits/>
